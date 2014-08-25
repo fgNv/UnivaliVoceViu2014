2f7dfaedfb8dce51ad85c47d6caf59dd20ae5fc6
RicardoAnz <stroke666@gmail.com>
2014-08-25 20:41:35 -0300

    implemServicoAnunciantes

---
+++ VoceViuModel/Users/Services/AdvertiserService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VoceViuModel.Users.Abstractions;
+
+namespace VoceViuModel.Users.Services
+{
+    public class AdvertiserService
+    {
+        private readonly IAdvertiserRepository _advertiserRepository;
+
+        public AdvertiserService(IAdvertiserRepository advertiserRepository)
+        {
+            _advertiserRepository = advertiserRepository;
+
+        }
+
+
+        public void Remove(int id)
+        {
+            var advertiser = _advertiserRepository.Get(id);
+            _advertiserRepository.Remove(advertiser);
+            _advertiserRepository.SaveChanges();
+        }
+    }
+}
